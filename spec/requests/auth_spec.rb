73b20486826329a7a61fb57dd6dba58c7bbe7482
Herby Raynaud <herby.raynaud@gmail.com>
2012-07-03 13:15:15 -0400

    Added test for closed season Added redirect to root path if not signed in instead of parent_root_path

--- spec/requests/auth_spec.rb
+++ spec/requests/auth_spec.rb
@@ -6,4 +6,12 @@ feature "Sign in and Sign Out of Work" do
     do_login(parent)
     current_path.should == parent_path(parent)
   end
+
+
+  scenario "Try to access site when current season is nil" do
+   Season.current.destroy if Season.current
+    parent = FactoryGirl.create(:complete_parent)
+    visit login_path
+    current_path.should == registration_closed_path
+  end
 end
