3d51163d8a7101f99b5125122e65a0457265910d
Herby Raynaud <herby.raynaud@gmail.com>
2012-07-06 18:54:28 -0400

    Added student path

--- app/admin/parents.rb
+++ app/admin/parents.rb
@@ -1,19 +1,27 @@
 ActiveAdmin.register Parent do
-  index do
+   index do
     column :first_name
     column :last_name
     column :email
     column :primary_phone
-    column :students do "parent"
-    link_to students_path(parent)
+    default_actions
+  end
+
+  show do |parent|
+    attributes_table do
+      row :name
+      row :email
+      row :full_address
+      row :primary_phone
+      row :secondary_phone
+      row :other_phone
+    end
+    panel "Students" do
+      table_for(parent.students) do |t|
+        t.column("Name") {|student| auto_link student.name        }
+        t.column("Currently Registerd?")   {|student| student.currently_registered? }
+      end
     end
   end
-
-
-  collection_action :students, :method => :get do |parent|
-    # Do some CSV importing work here...
-    @students = parent.students
-
-  end
-
+  
 end
