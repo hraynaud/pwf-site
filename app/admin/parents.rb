23826fd7e66f08dd5275224a2716e6b1f68417a6
Herby Raynaud <herby.raynaud@gmail.com>
2012-07-06 15:56:43 -0400

    configuring active admin

--- app/admin/parents.rb
+++ app/admin/parents.rb
@@ -1,3 +1,19 @@
 ActiveAdmin.register Parent do
-  
+  index do
+    column :first_name
+    column :last_name
+    column :email
+    column :primary_phone
+    column :students do "parent"
+    link_to students_path(parent)
+    end
+  end
+
+
+  collection_action :students, :method => :get do |parent|
+    # Do some CSV importing work here...
+    @students = parent.students
+
+  end
+
 end
--- app/admin/student_registrations.rb
+++ app/admin/student_registrations.rb
@@ -1,3 +1,14 @@
 ActiveAdmin.register StudentRegistration do
-  
+  index do
+
+    column :id
+    column :season do |reg|
+      reg.season.description
+    end
+    column :student
+    column "Status", :status_cd
+    column :grade
+    column "T-Shirt Size", :size_cd
+  end
+
 end
