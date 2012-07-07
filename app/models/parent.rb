da96dcbbff0c56a1322164e3b4ffd6b998cb1cec
Herby Raynaud <herby.raynaud@gmail.com>
2012-07-07 15:54:27 -0400

    added with_current_registrations scope to parent

--- app/models/parent.rb
+++ app/models/parent.rb
@@ -13,19 +13,22 @@ class Parent < ActiveRecord::Base
   validates :secondary_phone, :other_phone, :format => {:with => /\A(\d{3})-(\d{3})-(\d{4})\Z/, :message => "Please enter a phone numbers as: XXX-XXX-XXXX"}, :allow_blank => true
   validates :demographics, :presence => :true, :on => :update
 
-  scope :with_current_registrations, joins(:student_registrations)
+  def self.with_current_registrations
+    self.joins(:students => :student_registrations).where("student_registrations.season_id = ?", Season.current.id).uniq
+  end
 
 
   def registration_complete?
     address1 && city && state && zip && primary_phone
   end
 
+
   def name
     "#{first_name} #{last_name}"
   end
 
   def full_address
-     "#{address1} #{address2} #{city} #{state} #{zip}"
+    "#{address1} #{address2} #{city} #{state} #{zip}"
   end
 
   def current_student_registrations
