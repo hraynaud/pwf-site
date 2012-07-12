a4a98dcd6576646b70b0784b36d348b440af25aa
Herby Raynaud <herby.raynaud@gmail.com>
2012-07-12 08:54:55 -0400

    Change Factory to FactoryGirl

--- spec/models/season_spec.rb
+++ spec/models/season_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe Season do
   it "should be valid" do
-  season = Factory.create(:season)
+  season = FactoryGirl.create(:season)
   season.should be_valid
   end
 end
