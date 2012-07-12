1dcffb3bca7ade4be61ead29f640bd37e83bf5ee
Herby Raynaud <herby.raynaud@gmail.com>
2012-07-12 02:24:25 -0400

    Added dashboard to display current stats

--- app/admin/dashboards.rb
+++ app/admin/dashboards.rb
@@ -1,9 +1,16 @@
 ActiveAdmin::Dashboards.build do
 
+  section "Current Season Stats", :priority => 1 do
+    div do
+      render  "season_summary"
+    end
+  end
+
+
   # Define your dashboard sections here. Each block will be
   # rendered on the dashboard in the context of the view. So just
   # return the content which you would like to display.
-  
+
   # == Simple Dashboard Section
   # Here is an example of a simple dashboard section
   #
@@ -14,7 +21,7 @@ ActiveAdmin::Dashboards.build do
   #       end
   #     end
   #   end
-  
+
   # == Render Partial Section
   # The block is rendered within the context of the view, so you can
   # easily render a partial rather than build content in ruby.
@@ -24,7 +31,7 @@ ActiveAdmin::Dashboards.build do
   #       render 'recent_posts' # => this will render /app/views/admin/dashboard/_recent_posts.html.erb
   #     end
   #   end
-  
+
   # == Section Ordering
   # The dashboard sections are ordered by a given priority from top left to
   # bottom right. The default priority is 10. By giving a section numerically lower
@@ -34,7 +41,7 @@ ActiveAdmin::Dashboards.build do
   #   section "Recent User", :priority => 1
   #
   # Will render the "Recent Users" then the "Recent Posts" sections on the dashboard.
-  
+
   # == Conditionally Display
   # Provide a method name or Proc object to conditionally render a section at run time.
   #
