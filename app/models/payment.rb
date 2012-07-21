68fcc48eb5eb6e1e29434831baaa3a50adee3bac
Herby Raynaud <herby.raynaud@gmail.com>
2012-07-20 22:14:51 -0400

    refactored paypal related code into separate module cleaned up code

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,7 +1,9 @@
 class Payment < ActiveRecord::Base
+  include PayPalSupport
   belongs_to :parent
   has_many :student_registrations
-  attr_accessor :stripe_card_token, :paypal_payment_token, :email, :first_name, :last_name
+  attr_accessor :stripe_card_token, :paypal_payment_token, :email, :first_name, :last_name, :pay_with
+  attr_reader :redirect_uri, :popup_uri
 
   delegate :email, :name, :to => :parent, :prefix => true
 
@@ -10,40 +12,33 @@ class Payment < ActiveRecord::Base
   validates :identifier, uniqueness: true
   validates :parent, :presence => true
 
-  scope :recurring, where(recurring: true)
-  scope :digital,   where(digital: true)
-  scope :popup,     where(popup: true)
-
-  after_create :create_stripe_charge!, :if => :is_card_payment?
-
-  def create_stripe_charge!()
-    self.parent = get_parent
-    charge = Stripe::Charge.create(
-      :amount => (amount*100).to_i,
-      :currency => "usd",
-      :card => stripe_card_token, # obtained with Stripe.js
-      :description => invoice_details
-    )
-    self.completed = true
-    save
+  def save_with_stripe!
+    if valid?
+      charge = create_stripe_charge!
+      self.stripe_charge_id = charge.id
+      self.completed = true
+      save!
+    end
   rescue Stripe::InvalidRequestError => e
     logger.error "!!!! Stripe error while creating customer: #{e.message}"
     errors.add :base, "There was a problem with your credit card. #{e.message}"
     false
+  rescue => e
+    errors.add :base, "There was a problem with this payment. #{e.message}"
+    false
   end
 
-  #PAYPAL RELATED METHODS
-  def details
-    if recurring?
-      client.subscription(self.identifier)
-    else
-      client.details(self.token)
-    end
+  def create_stripe_charge!
+    Stripe::Charge.create(
+      :amount => (amount*100).to_i,
+      :currency => "usd",
+      :card => stripe_card_token, # obtained with Stripe.js
+      :description => item_description
+    )
   end
 
-  attr_reader :redirect_uri, :popup_uri
-  def setup_paypal!(return_url, cancel_url)
-    response = client.setup(
+  def save_with_paypal!(return_url, cancel_url)
+    response = paypal_client.setup(
       payment_request,
       return_url,
       cancel_url,
@@ -51,29 +46,33 @@ class Payment < ActiveRecord::Base
       no_shipping: self.digital?
     )
     self.token = response.token
-    self.save!
     @redirect_uri = response.redirect_uri
     @popup_uri = response.popup_uri
-    self
-  end
-
-  def cancel!
-    self.canceled = true
     self.save!
     self
+  rescue => e
+
+    errors.add :base, "There was a problem with this payment. #{e.message}"
+    false
   end
 
-  def complete!(payer_id = nil)
+  def paypal_complete!(payer_id = nil)
     if self.recurring?
-      response = client.subscribe!(self.token, recurring_request)
+      response = paypal_client.subscribe!(self.token, recurring_request)
       self.identifier = response.recurring.identifier
     else
-      response = client.checkout!(self.token, payer_id, payment_request)
+      response = paypal_client.checkout!(self.token, payer_id, payment_request)
       self.payer_id = payer_id
       self.identifier = response.payment_info.first.transaction_id
     end
-    self.parent = get_parent
     self.completed = true
+    self.save!
+    self
+  end
+
+
+  def cancel!
+    self.canceled = true
     self.save!
     self
   end
@@ -86,94 +85,37 @@ class Payment < ActiveRecord::Base
     completed
   end
 
-  def description
-    "Payment id:#{id} Date: #{created_at.strftime('%y-%m-%d %H:%M')} Amount: #{amount}"
-  end
-
-
-  #UNUSED METHODS IN THIS IMPLEMENTATION
-
-  def unsubscribe!
-    client.renew!(self.identifier, :Cancel)
-    self.cancel!
-  end
-
-  def goods_type
-    digital? ? :digital : :real
-  end
-
-  def payment_type
-    recurring? ? :recurring : :instant
-  end
-
-  def ux_type
-    popup? ? :popup : :redirect
+  def payments_for
+    studs = ""
+    student_registrations.each do |reg|
+      studs += reg.student_name + " --"
+    end
   end
 
 
   private
 
-  def invoice_details
-    "Peter Westbrook Foundation Registration: #{Season.current.description}\n Invoice: #{self.id}\n"
+  def item_description
+    "Peter Westbrook Foundation Registration: #{Season.current.description}\n #{parent.name}\n #{payments_for}"
   end
 
-  def get_parent()
-    if payment_method == "paypal"
-      cust_details = details
-      self.email = cust_details.payer.email
-      self.first_name = cust_details.payer.first_name
-      self.last_name = cust_details.payer.last_name
-    end
+
+  def recurring_payment_description
+    "Monthly charge for #{item_description}"
   end
+
+  def instant_payment_description
+    "Instant for #{item_description}"
+  end
+
 
   def is_card_payment?
-    payment_method=="card"
+    pay_with=="card"
   end
 
-
-
-  def client
-    Paypal::Express::Request.new PAYPAL_CONFIG
+  def is_paypal_payment?
+    !is_card_payment?
   end
 
-  DESCRIPTION = {
-    item: 'Volo Vitamins order payment',
-    instant: 'Volo Vitamins instant payment',
-    recurring: 'Volo Vitamins recurring order payment'
-  }
-
-  def payment_request
-    request_attributes = if self.recurring?
-                           {
-                             billing_type: :RecurringPayments,
-                             billing_agreement_description: DESCRIPTION[:recurring]
-                           }
-                         else
-                           item = {
-                             name: DESCRIPTION[:item],
-                             description: DESCRIPTION[:item],
-                             amount: self.amount
-                           }
-                           item[:category] = :Digital if self.digital?
-                           {
-                             amount: self.amount,
-                             description: DESCRIPTION[:instant],
-                             items: [item]
-                           }
-                         end
-    Paypal::Payment::Request.new request_attributes
-  end
-
-  def recurring_request
-    Paypal::Payment::Recurring.new(
-      start_date: Time.now,
-      description: DESCRIPTION[:recurring],
-      billing: {
-      period: :Month,
-      frequency: 1,
-      amount: self.amount
-    }
-    )
-  end
 
 end
