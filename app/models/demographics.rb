2f6307125968a58ad1234be7f74e1357dbac6d3a
Herby Raynaud <herby.raynaud@gmail.com>
2012-07-22 10:17:58 -0400

    added season validation and fixed enum names

--- app/models/demographics.rb
+++ app/models/demographics.rb
@@ -1,15 +1,16 @@
 class Demographics < ActiveRecord::Base
   belongs_to :parent
+  belongs_to :season
 
   attr_accessible :num_adults, :num_minors, :income_range_cd, :education_level_cd, :home_ownership_cd
-  validates :num_adults, :num_minors, :income_range_cd, :education_level_cd, :home_ownership_cd, :presence => true, :on => :create
+  validates :num_adults, :num_minors, :income_range_cd, :education_level_cd, :home_ownership_cd, :season, :presence => true
 
 
   DEGREE = %w(High\ School Associates Bachelors Masters Doctorate)
-  as_enum :degree, DEGREE.each_with_index.inject({}) {|h, (item,idx)| h[item]=idx; h}
+  as_enum :education_level, DEGREE.each_with_index.inject({}) {|h, (item,idx)| h[item]=idx; h}
 
   INCOME = %w(0-24,999 25,000-49,999 50,000-74,999 75,000-99,999, 100,000-124,999, 125,000+ )
-  as_enum :income, INCOME.each_with_index.inject({}) {|h, (item,idx)| h[item]=idx; h}
+  as_enum :income_range, INCOME.each_with_index.inject({}) {|h, (item,idx)| h[item]=idx; h}
 
-  as_enum :home_type, [:Own, :Rent, :Other]
+  as_enum :home_ownership, [:Own, :Rent, :Other]
 end
