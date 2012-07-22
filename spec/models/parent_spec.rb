35222ea2caa927eb04416c8cf9bc01ff78c794d9
Herby Raynaud <herby.raynaud@gmail.com>
2012-07-22 10:19:52 -0400

    added specs for validating parent when demographics is invalid

--- spec/models/parent_spec.rb
+++ spec/models/parent_spec.rb
@@ -3,27 +3,37 @@ require 'spec_helper'
 describe Parent do
   it "should be valid" do
     parent = FactoryGirl.create(:parent)
-    parent.valid?(:create).should be_true
-    parent.registration_complete?.should be_false
+    parent.valid?.should be_false
+    # parent.registration_complete?.should be_false
+  end
+
+  it "should be invalid when season is nil on assocaiated demographic" do
+    parent = FactoryGirl.build(:parent_with_no_season_demographics )
+    parent.all_valid?.should be_false
+  end
+
+  it "should be invalid if associated demographic is invalid" do
+    parent = FactoryGirl.build(:parent_with_invalid_demographics )
+    parent.all_valid?.should be_false
   end
 
   it "should be valid" do
     parent = FactoryGirl.create(:complete_parent)
-    parent.valid?(:create).should be_true
+    parent.valid?.should be_true
     parent.registration_complete?.should be_true
   end
 
   describe "has_unpaid_pending_registrations" do
-    it "should say yes if there are pending registrations" do
-        parent = FactoryGirl.create(:parent_with_current_student_registrations)
-        parent.has_unpaid_pending_registrations?.should be_true
+    it "should be true if there are pending registrations" do
+      parent = FactoryGirl.create(:parent_with_current_student_registrations)
+      parent.has_unpaid_pending_registrations?.should be_true
     end
   end
 
- describe "current_unpaid_pending_registrations" do
+  describe "current_unpaid_pending_registrations" do
     it "should show count of pending registrations" do
-        parent = FactoryGirl.create(:parent_with_current_student_registrations)
-        parent.current_unpaid_pending_registrations.count.should == 2
+      parent = FactoryGirl.create(:parent_with_current_student_registrations)
+      parent.current_unpaid_pending_registrations.count.should == 2
     end
   end
 
