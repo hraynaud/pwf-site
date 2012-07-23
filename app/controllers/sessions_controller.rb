27ae3381f650ef84df950890483bd221615f19fd
Herby Raynaud <herby.raynaud@gmail.com>
2012-07-23 15:35:32 -0400

    added devise session controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,9 @@
+class SessionsController < Devise::SessionsController
+ # POST /resource/sign_in
+  def create
+    resource = warden.authenticate!(auth_options)
+    set_flash_message(:notice, :signed_in) if is_navigational_format?
+    sign_in(resource_name, resource)
+   redirect_to after_sign_in_path_for(resource)
+  end
+end
