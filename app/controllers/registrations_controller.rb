a81efef8a1f43afefcb7f144f29802e7efb7fc23
Herby Raynaud <herby.raynaud@gmail.com>
2012-07-23 08:31:11 -0400

    refactored parent construction to ensure valid parent creation with all association dependencies

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -10,19 +10,19 @@ class RegistrationsController < Devise::RegistrationsController
     session[:parent_params].deep_merge!(params[:parent]) if params[:parent]
     @parent = Parent.new(session[:parent_params])
     @parent.current_step = session[:parent_step]
-    if @parent.valid?
+     if @parent.valid?
       if params[:back_button]
         @parent.previous_step
       elsif @parent.last_step?
-        @parent.save if @parent.all_valid?
+         @parent.save
       else
         @parent.next_step
       end
       session[:parent_step] = @parent.current_step
-    end
+     end
 
     if @parent.new_record?
-      @parent.build_demographics if demographics_needed?
+      @parent.demographics.build if demographics_needed?
       render "new"
     else
       session[:parent_step] = session[:parent_params] = nil
