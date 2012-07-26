fc27381a3f95098f8c3f1101c27c3c38db90c03f
Herby Raynaud <herby.raynaud@gmail.com>
2012-07-26 17:30:57 -0400

    Force ssl in development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,6 +35,9 @@ Pwf::Application.configure do
   # Expands the lines which load the assets
   config.assets.debug = true
 
+  # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
+  config.force_ssl = true
+
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
