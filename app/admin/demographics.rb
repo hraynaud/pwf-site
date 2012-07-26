8ac221e1bf1fff8628f9bc205d40be030faebcd3
Herby Raynaud <herby.raynaud@gmail.com>
2012-07-26 17:30:04 -0400

    Fixed demographics admin page

--- app/admin/demographics.rb
+++ app/admin/demographics.rb
@@ -5,9 +5,17 @@ ActiveAdmin.register Demographic do
 
   index do
     column :parent
-    column :income_range
-    column :education_level
-    column :home_ownership
+    column "Income Range", :income_range_cd, :sortable => :income_range_cd do |d|
+      d.income_range
+    end
+
+column "Education Level", :education_level_cd, :sortable => :education_level_cd do |d|
+      d.education_level
+    end
+
+    column "Home Ownership", :home_ownership_cd, :sortable => :home_ownership_cd do|d|
+      d.home_ownership
+    end
     column :num_minors
     column :num_adults
     column :season
