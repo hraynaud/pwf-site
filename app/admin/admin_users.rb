bd39eacb3788989e49ab8eb2a90a4bf5f98b71d0
Herby Raynaud <herby.raynaud@gmail.com>
2012-08-04 10:11:07 -0400

    Added Admin user resource to admin panel

---
+++ app/admin/admin_users.rb
@@ -0,0 +1,26 @@
+ActiveAdmin.register AdminUser do
+  filter :email
+
+  index do
+    column :email
+    default_actions
+  end
+
+  show :title => proc {"#{@admin_user.email}"} do |admin|
+    attributes_table do
+      row :email
+      row :id
+    end
+  end
+
+
+  form do |f|
+    f.inputs f.object.email do
+      f.input :email
+      f.input :password
+      f.input :password_confirmation
+    end
+    f.buttons :commit
+  end
+
+end
