577623e1fff50bd49045ad95b4253d26132d63bd
Herby Raynaud <herby.raynaud@gmail.com>
2012-09-03 15:44:00 -0400

    Added styling and js to select attendees

--- vendor/assets/javascripts/active_admin.js
+++ vendor/assets/javascripts/active_admin.js
@@ -1,1 +1,16 @@
 //= require active_admin/base
+
+$(document).ready(function() {
+  $("input[type=checkbox]:checked").prev().prev().css("background-color",$("#present-key").css("background-color"));
+  $(".attendance").click(
+    function() {
+      var id = $(this).attr("for");
+      if ($("#"+id).is(':checked')) {
+        //switching from selected to unselected
+        $(this).css("background-color",$("#absent-key").css("background-color"));
+      }
+      else {
+        $(this).css("background-color",$("#present-key").css("background-color"));
+      }
+    });
+});
