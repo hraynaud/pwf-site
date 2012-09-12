db85848707a63797ec375c2434a4ec664394de60
Herby Raynaud <herby.raynaud@gmail.com>
2012-09-12 09:50:39 -0400

    Added validation rules

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -1,4 +1,6 @@
 class Attendance < ActiveRecord::Base
   belongs_to :attendance_sheet
   belongs_to :student_registration, :include => :student
+
+  validates_uniqueness_of :student_registration_id, :scope=>[:session_date, :attendance_sheet_id]
 end
--- app/models/attendance_sheet.rb
+++ app/models/attendance_sheet.rb
@@ -1,5 +1,7 @@
 class AttendanceSheet < ActiveRecord::Base
-  has_many :attendances, :include => :student_registration
+  has_many :attendances, :include => :student_registration, :dependent => :destroy
   attr_accessible :session_date, :attendances_attributes
   accepts_nested_attributes_for :attendances
+
+  validates_uniqueness_of :session_date
 end
