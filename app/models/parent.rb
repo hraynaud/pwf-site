3bb3daaeb81f36af297973bce8a63d265316c419
Herby Raynaud <herby.raynaud@gmail.com>
2013-07-01 23:30:29 -0400

    Change name of associated model to demographics

--- app/models/parent.rb
+++ app/models/parent.rb
@@ -16,7 +16,7 @@ class Parent < ActiveRecord::Base
   validates :first_name, :last_name, :address1, :city, :state, :zip, :primary_phone,  :presence => true, :if => :on_contact_step?
   validates :primary_phone, :format => {:with =>/\A(\d{3})-(\d{3})-(\d{4})\Z/, :message => "Please enter a phone numbers as: XXX-XXX-XXXX"}, :if => :on_contact_step?
   validates :secondary_phone, :other_phone, :format => {:with => /\A(\d{3})-(\d{3})-(\d{4})\Z/, :message => "Please enter a phone numbers as: XXX-XXX-XXXX"}, :allow_blank => true
-  validates :current_household_profile, :presence => true, :if => :should_validate_household?
+  validates :demographics, :presence => true, :if => :should_validate_household?
   # validate :all_valid?, :on => :save
 
   #TODO This scope format below is more efficient but a bug in AA prevents it use. When the next release is available change the scope
