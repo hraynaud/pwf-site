806ead7117ff59e07ac2df7d7a95c56e517d24a7
Herby Raynaud <herby.raynaud@gmail.com>
2013-09-05 17:57:51 -0400

    Adds back the user id to parent table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130904012515) do
+ActiveRecord::Schema.define(:version => 20130905215625) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -120,6 +120,7 @@ ActiveRecord::Schema.define(:version => 20130904012515) do
   create_table "parents", :force => true do |t|
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.integer  "user_id"
   end
 
   create_table "payments", :force => true do |t|
