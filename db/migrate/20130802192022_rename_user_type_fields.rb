ea31a7633b77dff7fe8a97a5762381bc4663d247
Herby Raynaud <herby.raynaud@gmail.com>
2013-09-20 08:46:17 -0400

    Skips validation in data migrtion

--- db/migrate/20130802192022_rename_user_type_fields.rb
+++ db/migrate/20130802192022_rename_user_type_fields.rb
@@ -1,8 +1,8 @@
 class RenameUserTypeFields < ActiveRecord::Migration
- def change
-   rename_column :users, :admin, :is_mgr
-   rename_column :users, :parent, :is_parent
-   rename_column :users, :tutor, :is_tutor
+  def change
+    rename_column :users, :admin, :is_mgr
+    rename_column :users, :parent, :is_parent
+    rename_column :users, :tutor, :is_tutor
     User.all.each do |user|
       user.update_attribute(:is_parent, true)
       p = Parent.new
@@ -10,7 +10,8 @@ class RenameUserTypeFields < ActiveRecord::Migration
       user.profileable = p
       user.save
       Demographic.where(:parent_id => user.id).map{|d|d.update_attribute(:parent_id,  p.id)}
-      Student.where(:parent_id => user.id).map{|d|d.update_attribute(:parent_id,  p.id)}
-      Payment.where(:parent_id => user.id).map{|d|d.update_attribute(:parent_id,  p.id)}
- end
+      Student.where(:parent_id => user.id).map{|d|d.update_column(:parent_id,  p.id)}
+      Payment.where(:parent_id => user.id).map{|d|d.update_column(:parent_id,  p.id)}
+    end
+  end
 end
