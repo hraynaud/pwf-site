df678990e1fe7b7d4281fc351887ec1308baca0b
Herby Raynaud <herby.raynaud@gmail.com>
2013-11-17 18:14:26 -0500

    adds dtable class to tables

--- app/assets/javascripts/init_datables.js
+++ app/assets/javascripts/init_datables.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  $('.table').dataTable( {
+  $('.dtable').dataTable( {
     "sDom": "<'row'<'span4'l><'span6'f>r>t<'row'<'span6'i><'span6'p>>",
     "sPaginationType": "bootstrap",
     "oLanguage": {
