a447842aa09fcabd174e843616373a76c6b4765d
hraynaud <herby.raynaud@gmail.com>
2014-10-26 21:22:11 -0400

    make report card use report_card resource unnested

--- app/controllers/report_cards_controller.rb
+++ app/controllers/report_cards_controller.rb
@@ -2,44 +2,42 @@ class ReportCardsController < ApplicationController
 	respond_to :html
 
   before_filter :require_parent_user
-  before_filter :get_student_registration
-  before_filter :get_report_card, only: [:edit, :update, :destroy, :transcript]
- 
+  before_filter :get_student_registration, only: [:new]
+  before_filter :get_report_card, except: [:index, :new, :create]
+
   def index
     @report_cards = current_parent.report_cards
   end
 
   def new
-    # @student_registrations =current_parent.student_registrations.current
     @report_card = ReportCard.new
     @grade_range =  GradeRanger.default_grade_range 
     @validations= GradeRanger.default_validations
 		@student_registration = current_parent.student_registrations.find( params[:student_registration_id])
-    # params[:student_id] ? @selected = @student_registrations.find_by_student_id( params[:student_id]).id : nil 
   end
 
   def show
-    @report_card = current_parent.report_cards.find(params[:id])
     @uploader = @report_card.transcript
     @uploader.key = key
     @uploader.success_action_redirect = transcript_report_card_url(@report_card)
   end
 
   def create
-		@report_card = @student_registration.report_cards.create(params[:report_card])
+		@report_card = @current_parent.report_cards.create(params[:report_card])
     if @report_card.valid?
       flash[:notice]="Report card template created. Please upload a hard copy to complete"
     else
       flash[:error]="Some errors were detected please try again"
     end
-		respond_with @student_registration, @report_card
+		respond_with @report_card
   end
 
 
   def edit
-      @uploader = @report_card.transcript
-      @uploader.key = key
-      @uploader.success_action_redirect = transcript_report_card_url(@report_card)
+		@student_registration = @report_card.student_registration 
+		@uploader = @report_card.transcript
+		@uploader.key = key
+		@uploader.success_action_redirect = transcript_report_card_url(@report_card)
   end
 
   def update
@@ -49,6 +47,7 @@ class ReportCardsController < ApplicationController
     else
 			flash[:error]="Error saving report card"
     end
+		respond_with @report_card
   end
 
   def transcript
@@ -70,7 +69,8 @@ class ReportCardsController < ApplicationController
  end 
 
  def get_report_card
-	 @report_card =  @student_registration.report_cards.find(params[:id])
+
+    @report_card = current_parent.report_cards.find(params[:id])
  end 
 
 end
