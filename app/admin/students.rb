240ee056ca8d4a341d40baa484eff36207be13c1
hraynaud <herby.raynaud@gmail.com>
2015-10-30 10:01:15 -0400

    Adds ethnicity to student admin page

--- app/admin/students.rb
+++ app/admin/students.rb
@@ -21,6 +21,7 @@ ActiveAdmin.register Student do
     f.inputs  do
       f.input :first_name
       f.input :last_name
+      f.input :ethnicity, :collection =>Student::ETHNICITY, :input_html => {:id => "ethnic"}, :label => "Ethnicity"
       f.input :gender, :as => :select, :collection => ['M', 'F']
       f.input :dob
       f.input :parent, :collection => Parent.joins(:user).with_current_registrations.order("users.last_name asc, users.first_name asc").map{|p| [p.name, p.id]}
