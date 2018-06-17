bbf6ffefb1ee76fd252cfffafd09445c72fab8f7
Herby Raynaud <herby@yieldmo.com>
2018-06-17 12:49:13 -0400

    Changes user to STI table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_17_000715) do
+ActiveRecord::Schema.define(version: 2018_06_17_144920) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -415,6 +415,7 @@ ActiveRecord::Schema.define(version: 2018_06_17_000715) do
     t.integer "profileable_id"
     t.string "profileable_type", limit: 255
     t.boolean "is_instructor"
+    t.string "type"
     t.index ["email"], name: "index_parents_on_email", unique: true
     t.index ["reset_password_token"], name: "index_parents_on_reset_password_token", unique: true
   end
