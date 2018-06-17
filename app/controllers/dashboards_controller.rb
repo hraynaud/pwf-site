77acf26665f29e9549b8e7c1ce16232e9908c9aa
Herby Raynaud <herby@yieldmo.com>
2018-06-17 12:58:09 -0400

    removes polymorphic relationship on user

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,7 +1,7 @@
 class DashboardsController < ApplicationController
 
   def show
-    user = current_user.profileable
+    user = current_user
     type = user.class.name.downcase
     self.instance_variable_set("@#{type}", user)
     render "#{type}"
