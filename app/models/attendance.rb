2ddf2c422cab44a0a11889d5373a917396edd49c
Herby Raynaud <herby@yieldmo.com>
2018-07-01 14:56:51 -0400

    Fixes n+1 queries

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -1,7 +1,7 @@
 class Attendance < ApplicationRecord
   belongs_to :attendance_sheet
-  belongs_to :student_registration, ->{includes  :student}
-  has_one :student, through: :student_registration
+  belongs_to :student_registration, ->{includes :student}
+  has_one :student,  through: :student_registration
   has_one :group, through: :student_registration
 
   validates_uniqueness_of :student_registration_id, :scope=>[:session_date, :attendance_sheet_id]
