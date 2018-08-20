25ea3ea3b7bc6cb4d4f7f2990f42083258f741e7
Herby Raynaud <herby@yieldmo.com>
2018-08-19 23:09:09 -0400

    removes PayPal support

--- app/assets/javascripts/site/features/payments.js
+++ app/assets/javascripts/site/features/payments.js
@@ -3,30 +3,12 @@ var PWF = window.PWF || {modules:{}};
 PWF.modules["payments"] = (function(){
 
   var form = $("#payment-form")
-    , selectedPaymentType = $("input[name='payment[pay_with]']:checked").val()
-    , options = $("input[name='payment[pay_with]']")
-    , hiddenClass = "collapse"
-    , fields = $(".credit-card-fields")
-    , submitButton = $("#btn-submit")
     , paymentHandler
   ;
 
-  function initPaymentMethodToggler() {
-    options.change(function(event){
-      var opt = event.target;
-
-      if(opt.id === "payment_pay_with_paypal"){
-        fields.addClass(hiddenClass);
-      }else{
-        fields.removeClass(hiddenClass);
-        paymentHandler = new PWF.StripePaymentHandler(form);
-        paymentHandler.init();
-      }
-    });
-  }
-
   function init(){
-    initPaymentMethodToggler();
+    paymentHandler = new PWF.StripePaymentHandler(form);
+    paymentHandler.init();
   }
 
   return {
