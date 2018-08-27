b36b7c17a0fdfa4783372633e29ef060d57d5271
Herby Raynaud <herby@yieldmo.com>
2018-08-26 23:22:35 -0400

    specs cover most use cases

--- spec/models/payment_spec.rb
+++ spec/models/payment_spec.rb
@@ -3,7 +3,7 @@ require 'stripe'
 
 describe Payment do
   describe "#save" do
-    context "when the model is valid" do
+    context "when the model is valid and stripe payment succeeds" do
       let(:payment){FactoryBot.build(:stripe_payment)}
       let(:parent){mock_model(Parent).as_null_object}
 
@@ -12,29 +12,44 @@ describe Payment do
         stub_stripe
       end
 
-      context "succeeds" do
-        it "saves a new payment" do
+      context "And stripe charge is successful" do
+        it "creates a new payment" do
           expect{payment.save}.to change{Payment.count}.by 1
         end
 
-        it "should update the student registrations" do
+        it "applies payments to student registrations when program is fencing " do
           parent = FactoryBot.create(:parent_with_current_student_registrations)
-          payment = FactoryBot.build(:stripe_payment, :parent =>parent, :program => :fencing )
+
+          payment = FactoryBot.build(:stripe_payment, parent: parent, :program => :fencing )
           expect{payment.save;payment.reload}.to change{payment.paid_fencing_registrations.count}.from(0).to(2)
         end
 
-        it "should update the aep registrations" do
+        it "doesn't apply payment if registrations are not pending" do
+          parent = FactoryBot.create(:parent_with_current_student_registrations)
+          parent.student_registrations.each{|r|r.confirmed_fee_waived!; r.save}
+          payment = FactoryBot.build(:stripe_payment, parent: parent, :program => :fencing )
+          expect{payment.save;payment.reload}.to_not change{payment.paid_fencing_registrations.count}
+        end
+
+        it "doesn't apply payment if registrations are not currrent" do
+          parent = FactoryBot.create(:parent)
+          FactoryBot.create_list(:student_registration, 2, :previous, parent: parent)
+          payment = FactoryBot.build(:stripe_payment, parent: parent, :program => :fencing )
+          expect{payment.save; payment.reload}.to_not change{payment.paid_fencing_registrations.count}
+        end
+
+        it "applies payments to AEP registrations when program is aep " do
           parent = FactoryBot.create(:parent_with_current_student_registrations)
           regs = parent.student_registrations
           aeps = []
           regs.each{|r|aeps.push r.create_aep_registration}
-          payment = FactoryBot.build(:stripe_payment, :parent =>parent, :program => :aep )
+          payment = FactoryBot.build(:stripe_payment, parent: parent, :program => :aep )
           expect{payment.save;payment.reload}.to change{payment.paid_aep_registrations.count}.from(0).to(2)
         end
       end
 
       context "when stripe fails" do
-        it "does not save a new payment" do
+        it "does create new payment" do
           stub_stripe_to_raise_exception
           payment.save
           Payment.count.should == 0
