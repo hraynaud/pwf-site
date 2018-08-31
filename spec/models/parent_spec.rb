cf6dd8ab83eeadc21de12926005ae68aa13b95ce
Herby Raynaud <herby@yieldmo.com>
2018-08-31 00:33:31 -0400

    Fixes spec syntax

--- spec/models/parent_spec.rb
+++ spec/models/parent_spec.rb
@@ -1,22 +1,23 @@
 require 'spec_helper'
 
 describe Parent do
+
   it "valid with current demographic profile" do
-    parent = FactoryBot.build(:parent_with_current_demographic_profile)
+    parent = FactoryBot.build(:parent)
     expect(parent.valid?).to be true
   end
 
   describe "has_current_unpaid_fencing_registrations" do
     it "should be true if there are pending registrations" do
       parent = FactoryBot.create(:parent_with_current_student_registrations)
-      parent.has_current_unpaid_fencing_registrations?.should be_true
+      expect(parent.has_current_unpaid_fencing_registrations?).to be true
     end
   end
 
-  describe "current_unpaid_pending_registrations" do
+  describe "current_unpaid_registrations" do
     it "should show count of pending registrations" do
       parent = FactoryBot.create(:parent_with_current_student_registrations)
-      parent.current_unpaid_pending_registrations.count.should == 2
+      expect(parent.unpaid_registrations.count).to eq 2
     end
   end
 
