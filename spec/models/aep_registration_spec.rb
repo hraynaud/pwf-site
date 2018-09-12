b528e99716b7913a7960199432a668a76c1af6f1
Herby Raynaud <herby@yieldmo.com>
2018-09-11 23:13:10 -0400

    all test passing

--- spec/models/aep_registration_spec.rb
+++ spec/models/aep_registration_spec.rb
@@ -1,13 +1,14 @@
 
 describe AepRegistration, :focus => :aep_fee do
   it "is invalid if student_registration is not confirmed" do
-    expect{FactoryBot.create(:aep_registration )}.to raise_error(ActiveRecord::RecordInvalid,'Validation failed: Cannot register unconfirmed student for AEP')
+    student_reg =FactoryBot.create(:student_registration, status_cd: 0)
+    expect{FactoryBot.create(:aep_registration, student_registration: student_reg)}.to raise_error(ActiveRecord::RecordInvalid,'Validation failed: Cannot register unconfirmed student for AEP')
   end
 
-    it "is valid if student_registration is confirmed" do
-      reg = FactoryBot.create(:student_registration,  :confirmed)
-      expect{FactoryBot.create(:aep_registration, student_registration: reg)}.to change{AepRegistration.count}.by(1)
-    end
+  it "is valid if student_registration is confirmed" do
+    reg = FactoryBot.create(:student_registration,  :confirmed)
+    expect{FactoryBot.create(:aep_registration, student_registration: reg)}.to change{AepRegistration.count}.by(1)
+  end
 
 
 end
