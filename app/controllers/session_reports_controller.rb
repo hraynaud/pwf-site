ce16a6f228cca8a706b3dc937c2ddc5d59c2d1d8
Herby Raynaud <herby@yieldmo.com>
2018-09-17 19:28:49 -0400

    Removes session reports feature

--- app/controllers/session_reports_controller.rb
+++
@@ -1,9 +0,0 @@
-class SessionReportsController < TutorReportsController
-
- 
- def session_report_params
-
-   params.require(:session_report).permit(:aep_registration_id, :worked_on_other, :session_date,:confirmed, 
-    :worked_on, :preparation, :participation, :comprehension, :motivation, :comments)
- end 
-end
