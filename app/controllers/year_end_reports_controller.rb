b2cfe7de4120387ec68c3131ded81c24785e8d18
Herby Raynaud <herby@yieldmo.com>
2018-09-17 23:40:21 -0400

    Removes deprecated feature

--- app/controllers/year_end_reports_controller.rb
+++
@@ -1,7 +0,0 @@
-class YearEndReportsController < TutorReportsController 
-
-
- def year_end_report_params 
-   params.require(:year_end_report).permit(:academic_skills, :achievements, :attendance, 
-    :challenges_concerns, :comments, :participation, :preparation, :recommendations)
-end
--- app/models/student_assessment.rb
+++
@@ -1,2 +0,0 @@
-class StudentAssessment < ApplicationRecord
-end
