63d484eb47919ca808d8088ca85eb89b73e23195
Herby Raynaud <herby@yieldmo.com>
2018-10-14 17:12:23 -0400

    Limit report cards to Fall/Winter/ Spring Summer

--- app/models/marking_period.rb
+++ app/models/marking_period.rb
@@ -4,6 +4,10 @@ class MarkingPeriod < ApplicationRecord
     @periods ||= self.order(:name).inject({}){|a,v| a[v.id]=v.name;a}
   end
 
+  def self.simple_periods
+     where(name: ["Fall/Winter", "Spring/Summer"])
+  end
+
   def self.name_for id
     periods[id]
   end
