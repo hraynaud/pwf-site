4bab6075097b9ee4358698a7beb9a959f9b94b62
Herby Raynaud <herby@yieldmo.com>
2018-10-17 09:00:41 -0400

    Refactors attendance pdf

--- app/pdfs/attendance_sheet_pdf.rb
+++ app/pdfs/attendance_sheet_pdf.rb
@@ -1,45 +1,61 @@
 class AttendanceSheetPdf <Prawn::Document
-  NAMES_PER_PAGE = 54.0
+  NAMES_PER_PAGE = 50.0
 
-  def initialize(attendance_sheet, date)
+  CHECK_BOX_TEXT = "      "
+
+  def initialize(sheet, date)
     super()
     @date = date
-    @students = attendance_sheet
-    p = 0;
-    draw_page
+    @enrollment = sheet
+    @num_students = sheet.size
+    @student_data = sheet.map{|enrolled|[name_with_attendence(enrolled), "     "]}
+    @pages = (@num_students/NAMES_PER_PAGE).ceil
+    draw_sheets
+
   end
 
   def missing_rc
     @arr ||= ENV["BAD_RC"].split(",").map(&:to_i)
   end
 
- def missing_indicator id
-   id.in?(missing_rc) ? "<b>*</b>" : ""
- end
+  def missing_indicator id
+    id.in?(missing_rc) ? "<b>*</b>" : ""
+  end
 
-
-  def draw_page
-
-    data = @students.map{|n|["#{n.first_name.titleize} #{n.last_name.titleize}", " "*12]}
-    count = @students.size
-    slices = (count/NAMES_PER_PAGE).ceil
-
-    0.upto(slices-1) do|slice|
-      idx = slice * NAMES_PER_PAGE
-      chunk = data.slice(idx, NAMES_PER_PAGE)
-
-      print_page_header with_starting_and_ending_student_listed_on_page(chunk)
-
-      column_box([0, cursor], :columns => 2, :width => bounds.width) do
-        table chunk, :cell_style => { :inline_format => true }
-      end
-
+  def draw_sheets
+    0.upto(@pages - 1) do |page|
+      batch = assemble_batch_for page
+      print_batch batch
       start_new_page
     end
   end
 
-  def with_starting_and_ending_student_listed_on_page chunk
-    {starting: chunk.first.first, ending: chunk.last.first}
+  def print_batch batch
+      print_page_header batch_description(batch)
+      column_box([0, cursor], :columns => 2, :width => bounds.width) do
+        table batch, :cell_style => { :inline_format => true }
+      end
+  end
+
+  def assemble_batch_for page
+    offset = page * NAMES_PER_PAGE
+    @student_data.slice(offset, NAMES_PER_PAGE)
+  end
+
+  def name_with_attendence enrollee
+    "#{enrollee.first_name.titleize} #{enrollee.last_name.titleize}:  #{current_attendances enrollee}"
+  end
+
+  def current_attendances enrollee
+    enrollee.student.attendances.current.present.count
+  end
+
+  def batch_description batch
+    with_starting_and_ending_student_listed_on_page batch
+  end
+
+  def with_starting_and_ending_student_listed_on_page batch
+    {starting: batch.first.first, ending: batch.last.first}
   end
 
   def print_page_header header_name
