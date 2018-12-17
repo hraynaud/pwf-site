f96bbc6530f24f62f07009a4b13e9037c27b09ef
Herby Raynaud <herby@yieldmo.com>
2018-12-17 12:05:03 -0500

    WIP enum handling in views

--- app/models/demographic.rb
+++ app/models/demographic.rb
@@ -5,16 +5,14 @@ class Demographic < ApplicationRecord
   validates :num_adults, :num_minors, :income_range_cd, :education_level_cd, :home_ownership_cd, :season, :presence => true
 
 
-  DEGREE = %w(High\ School Associates Bachelors Masters Doctorate)
-  as_enum :education_level, DEGREE.map{|v| v.parameterize.underscore.to_sym}, pluralize_scopes:false 
+  DEGREE = ["High School", "Associates",  "Bachelors",  "Masters",  "Doctorate"]
+  as_enum :education_level, DEGREE
 
-  INCOME = %w(0-24,999 25,000-49,999 50,000-74,999 75,000-99,999, 100,000-124,999, 125,000+ )
-  as_enum :income_range, INCOME.map{|v| v.parameterize.underscore.to_sym}, pluralize_scopes:false 
-
+  INCOME = ["$0 - $24 999",  "$25,000 - $49,999",  "$50,000 - $74,9999",  "$75,000 - $99,999",  "$100,000 - $124,999",  "125, 000+" ]
+  as_enum :income_range, INCOME
 
   as_enum :home_ownership, [:Own, :Rent, :Other]
   before_validation :set_season
-
 
   scope :current, ->(){where(season_id: Season.current_season_id)}
 
