e3f77be29279bbe9d6f6ba7f13c6719339903d6d
Herby Raynaud <herby@yieldmo.com>
2019-01-21 21:49:38 -0500

    use sangrid for mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,11 +81,11 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { :host => ENV['DEFAULT_HOST_URL'] }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    :address              => "smtp.gmail.com",
+    :address              => 'smtp.sendgrid.net',
     :port                 => 587,
     :domain               => 'petewestbrook.org',
-    :user_name            => 'registrations@peterwestbrook.org',
-    :password             => ENV['SMTP_PASSWORD'],
+    :user_name            => ENV['SENDGRID_USERNAME'],
+    :password             => ENV['SENDGRID_PASSWORD'],
     :authentication       => 'plain',
     :enable_starttls_auto => true  }
 
