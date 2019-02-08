6a1be79615338f2fa22b5308063443782ad551ba
Herby Raynaud <herby@yieldmo.com>
2019-02-08 13:47:40 -0500

    Fixes issue with phone number validation and password reset

--- app/models/contact_detail.rb
+++ app/models/contact_detail.rb
@@ -1,8 +1,19 @@
 class ContactDetail < ApplicationRecord
   belongs_to :user
   validates_presence_of :user
+  
+  PHONE_REGEX= /\A\(?\d{3}\)?[- ]?\d{3}[- ]?\d{4}\z/
+  PHONE_MESSAGE = "Please enter a valid phone number"
 
   validates  :address1, :city, :state, :zip, :primary_phone,  :presence => true
-  validates :primary_phone, :format => {:with =>/\A(\d{3})-(\d{3})-(\d{4})\Z/, :message => "Please enter a phone numbers as: XXX-XXX-XXXX"}
-  validates :secondary_phone, :other_phone, :format => {:with => /\A(\d{3})-(\d{3})-(\d{4})\Z/, :message => "Please enter a phone numbers as: XXX-XXX-XXXX"}, :allow_blank => true
+  validates :primary_phone, :format => {:with => PHONE_REGEX, :message => PHONE_MESSAGE}
+  validates :secondary_phone, :other_phone, :format => {:with => PHONE_REGEX, :message => PHONE_MESSAGE}, :allow_blank => true
+
+
+  def is_valid_phone
+
+  end
+
+
+
 end
--- app/models/report_card.rb
+++ app/models/report_card.rb
@@ -14,7 +14,7 @@ class ReportCard < ApplicationRecord
   validates :student_registration, :academic_year, :marking_period, presence: true
   validate  :transcript_uploaded
 
-  scope :current, ->{joins(:student_registration).merge(StudentRegistration.current.confirmed)}
+  scope :current, ->{includes(:student_registration).references(:student_registration).where("student_registrations.season_id = ?", Season.current.id)}
   scope :previous, ->{joins(:student_registration).where.not("student_registrations.id =?", Season.current)}
   scope :graded, ->{includes(:grades).references(:grades).where.not(grades: {id: nil})}
   scope :not_graded, ->{includes(:grades).references(:grades).where(grades: {id: nil})}
