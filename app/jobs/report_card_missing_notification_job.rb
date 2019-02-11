e45fc5a611e0c075a9089ea473686981defdd246
Herby Raynaud <herby@yieldmo.com>
2019-02-11 01:56:17 -0500

    Change back to deliver_later to target mailer queue in reds

--- app/jobs/report_card_missing_notification_job.rb
+++ app/jobs/report_card_missing_notification_job.rb
@@ -3,7 +3,7 @@ class ReportCardMissingNotificationJob < ApplicationJob
   def perform args
     params = JSON.parse(args)
     recipients(params['exclude_list']).each do |student|
-      ReportCardMailer.missing(student, params['subject'], params['message']).deliver
+      ReportCardMailer.missing(student, params['subject'], params['message']).deliver_later
     end
   end
 
