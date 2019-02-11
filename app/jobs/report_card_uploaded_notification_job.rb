0e774f4414d0ae391a2193c06533d679cee71f62
Herby Raynaud <herby@yieldmo.com>
2019-02-10 22:27:35 -0500

    Queue jobs

--- app/jobs/report_card_uploaded_notification_job.rb
+++ app/jobs/report_card_uploaded_notification_job.rb
@@ -1,7 +1,7 @@
 class ReportCardUploadedNotificationJob < ApplicationJob
 
   def perform report_card
-    ReportCardMailer.uploaded(report_card).deliver
+    ReportCardMailer.uploaded(report_card).deliver_later
   end
 
 end
