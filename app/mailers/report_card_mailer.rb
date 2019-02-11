18bb78c94cd4e8bd77de39a74cf72a72d53b6d42
Herby Raynaud <herby@yieldmo.com>
2019-02-11 01:17:34 -0500

    Redirect to dummy email address in dev and non production environments

--- app/mailers/report_card_mailer.rb
+++ app/mailers/report_card_mailer.rb
@@ -7,19 +7,27 @@ class ReportCardMailer < ActionMailer::Base
     mail to: "report_card_uploads@peterwestbrook.org", subject: "New report card uploaded"
   end
 
-
   def confirmation card, user
     @name = user.name
     @report_card = card
     mail to: user.email, subject: "Report card upload confirmation"
   end
 
-
   def missing missing, subject, message
     @name = missing.parent_first_name
     @student = missing.student_name
     @term = missing.term
     @message = message
-    mail to: missing.parent_email, subject: @subject
+    mail to: email_address_for(missing), subject: @subject
+  end
+
+  def email_address_for missing
+    if ENV['BLOCK_MAILS'] || Rails.env.development?
+      "dummy-email@peterwestbrook.org"
+      
+      #Iisadummy
+    else
+      missing.parent_email
+    end
   end
 end
