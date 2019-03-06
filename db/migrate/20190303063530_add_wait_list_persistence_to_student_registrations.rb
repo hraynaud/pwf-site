aaaec443a06724622965667ec5a84f27be695126
Herby Raynaud <herby@yieldmo.com>
2019-03-05 22:19:17 -0500

    removes migration file

--- db/migrate/20190303063530_add_wait_list_persistence_to_student_registrations.rb
+++
@@ -1,5 +0,0 @@
-class AddWaitListPersistenceToStudentRegistrations < ActiveRecord::Migration[5.2]
-  def change
-    add_column :student_registrations, :keep_on_notify_if_waitlisted, :boolean
-  end
-end
