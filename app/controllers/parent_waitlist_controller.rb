e26a82e251ebe784587670226812cb16b4e78650
Herby Raynaud <herby@yieldmo.com>
2019-03-12 23:39:14 +0100

    Sets layout to false

--- app/controllers/parent_waitlist_controller.rb
+++ app/controllers/parent_waitlist_controller.rb
@@ -1,10 +1,10 @@
 class ParentWaitlistController < ParentsController
+  layout false
 
   def update
     @parent.assign_attributes(parent_params)
     if @parent.save
       redirect_to waitlist_updated_path, notice: "Settings updated. You will now be logged out"
-      
     else
       render :edit, layout: nil
     end
