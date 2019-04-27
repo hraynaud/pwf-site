82c1aa4d8c92912cb01b4c4b883ee43ed182812c
Herby Raynaud <herby@yieldmo.com>
2019-04-27 17:13:10 -0400

    Adds clear filters button

--- app/admin/year_end_rewards.rb
+++ app/admin/year_end_rewards.rb
@@ -9,19 +9,58 @@ ActiveAdmin.register StudentRegistration,  as: "Year End Rewards" do
     ['admin', Season.current.description]
   end
 
+  scope :all, default: true
+
   scope "Hoodies" do |regs|
-    regs.where(id: Attendance.attendence_ids_with_count_greater_than(11))
+    regs.where(id: Attendance.attendence_ids_with_count_greater_than( controller.hoodie_count))
   end
 
   scope "T-shirts" do |regs|
-    regs.where(id: Attendance.attendence_ids_with_count_greater_than(5))
+    regs.where(id: Attendance.attendence_ids_with_count_greater_than(controller.t_shirt_count))
   end
 
   controller do
+    def hoodie_count 
+      params.dig("q", "h_eq") || Season.current.min_attendance_count_for_hoodies
+    end
+
+    def t_shirt_count 
+      params.dig("q", "t_eq") || Season.current.min_attendance_count_for_t_shirts
+    end
+
     def scoped_collection
-      StudentRegistration.current.confirmed
+      StudentRegistration.current.confirmed.joins(:student)
     end
   end
+
+  sidebar :filter do
+
+    @hood_pct = controller.hoodie_count
+    @tee_pct = controller.t_shirt_count
+
+    form class: "filter_form", action: admin_year_end_rewards_path do
+
+      div class: "form-element-grp inline"do
+        label "Hoodies" do
+          input type: "text", name: "q[h_eq]", value: @hood_pct
+        end
+      end
+
+      div class: "form-element-grp inline"do
+        label "T-shirts " do
+          input type: "text", name: "q[t_eq]", value: @tee_pct
+        end
+      end
+
+      div class: "buttons" do
+
+        button "Filter", type: "submit"
+        a "Clear Filters", class: "clear_filters_btn",  href: "#"
+      end
+    end
+  end
+
+
 
   index do
     column :last_name, :sortable =>'students.last_name' do |reg|
