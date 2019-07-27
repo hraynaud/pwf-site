18b985dfa5f3df13c8eb4fb21ae0ea71431faf1b
Herby Raynaud <herby@yieldmo.com>
2019-07-27 17:20:03 -0400

    fixes typo in notification list

--- app/admin/general_notification.rb
+++ app/admin/general_notification.rb
@@ -35,7 +35,7 @@ ActiveAdmin.register_page "General Notification" do
           option "Wait Listed Students", value: NotificationService::WAIT_LIST, selected: NotificationService::WAIT_LIST == mailing_list
           option "AEP Students", value: NotificationService::AEP_ONLY, selected: NotificationService::AEP_ONLY == mailing_list
           option "Unrenewed Registrations", value: NotificationService::UNRENEWED_PARENTS, selected: NotificationService::UNRENEWED_PARENTS == mailing_list
-          option "Unrenewed Registrations", value: NotificationService::WAIT_LIST_PRIORITY, selected: NotificationService::WAIT_LIST_PRIORITY == mailing_list
+          option "Priority Waitlist", value: NotificationService::WAIT_LIST_PRIORITY, selected: NotificationService::WAIT_LIST_PRIORITY == mailing_list
         end
       end
 
