3d64714ff3e6ab5ac95f45ac8a8eb0a24f2bd563
Herby Raynaud <herby.raynaud@gmail.com>
2020-07-20 00:45:54 -0400

    Adds REDIS_URL back but keeps REDISTOGO for qa environment

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,8 @@
+#FIXME should only be using one Redis url env var
 Sidekiq.configure_server do |config|
-  config.redis = { size: 9, url: ENV['REDISTOGO_URL'] || 'redis://localhost:6379/0' }
+  config.redis = { size: 9, url: ENV['REDIS_URL'] || ENV['REDISTOGO_URL'] || 'redis://localhost:6379/0' }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { size: 1, url: ENV['REDISTOGO_URL'] || 'redis://localhost:6379/0' }
+  config.redis = { size: 1, url:  ENV['REDIS_URL'] || ENV['REDISTOGO_URL'] || 'redis://localhost:6379/0' }
 end
